5d06dc4a1c8340f3e1034914584d292f47343bd8
Jérôme Audoux <jerome.audoux@gmail.com>
2015-01-06 10:50:35 +0100

    Add STAR Homemade files for chimeras and junctions

---
+++ lib/CracTools/BenchCT/Analyzer/STAR/Chimera.pm
@@ -0,0 +1,58 @@
+use strict;
+use warnings;
+package CracTools::BenchCT::Analyzer::STAR::Chimera;
+# ABSTRACT: Analyze results from a software experiment over the simulated data handled by BenchCT
+#
+use parent 'CracTools::BenchCT::Analyzer';
+
+use CracTools::Utils;
+#use Data::Dumper;
+
+sub canCheck {
+  my $self = shift;
+  my $event_type = shift;
+  if($self->SUPER::canCheck($event_type) ||  $event_type eq 'chimera') {
+    return 1;
+  }
+  return 0;
+}
+
+sub _init {
+  my $self = shift;
+  my %args = @_;
+  my $chimera_file = $args{file};
+  my $chim_it = CracTools::Utils::getFileIterator(file => $chimera_file,
+    parsing_method => \&parseSTARChimeraLine,
+    header_regex => '^#'
+  );
+  while (my $chimera = $chim_it->()) {
+    my $true_chimera = $self->checker->isTrueChimera($chimera->{chr1},$chimera->{pos1},$chimera->{strand1},$chimera->{chr2},$chimera->{pos2},$chimera->{strand2});
+    
+    if($true_chimera) {
+      $self->getStats('chimera')->addTruePositive($true_chimera);
+    } else {
+      #print STDERR Dumper($chimera);
+      $self->getStats('chimera')->addFalsePositive();
+    }
+  }
+}
+
+=head2 parseSTARChimeraLine
+
+=cut
+
+sub parseSTARChimeraLine {
+  my $line = shift;
+  my($chr1,$pos1,$strand1,$chr2,$pos2,$strand2) = split("\t",$line);
+  my @ids = split(":",$ids);
+  return {
+    chr1 => $chr1,
+    pos1 => $pos1,
+    strand1 => CracTools::Utils::convertStrand($strand1),
+    chr2 => $chr2,
+    pos2 => $pos2,
+    strand2 => CracTools::Utils::convertStrand($strand2),
+  };
+}
+
+1;
---
+++ lib/CracTools/BenchCT/Analyzer/STAR/Junction.pm
@@ -0,0 +1,63 @@
+use strict;
+use warnings;
+package CracTools::BenchCT::Analyzer::STAR::Junction;
+# ABSTRACT: Analyze results from a software experiment over the simulated data handled by BenchCT
+#
+use parent 'CracTools::BenchCT::Analyzer';
+
+use CracTools::Utils;
+
+sub canCheck {
+  my $self = shift;
+  my $event_type = shift;
+  if($self->SUPER::canCheck($event_type) ||  $event_type eq 'splice') {
+    return 1;
+  }
+  return 0;
+}
+
+sub _init {
+  my $self = shift;
+  my %args = @_;
+  my $junction_file = $args{file};
+  my $splice_it = CracTools::Utils::getFileIterator(file => $junction_file,
+    parsing_method => \&parseSTARJunctionLine,
+    header_regex => '^#',
+  );
+  while (my $splice = $splice_it->()) {
+    my $true_splice = $self->checker->isTrueSplice(
+      $splice->{chr},
+      $splice->{start},
+      $splice->{end} - $splice->{start},
+      $splice->{strand},
+    );
+    
+    if($true_splice) {
+      $self->getStats('splice')->addTruePositive($true_splice);
+    } else {
+      $self->getStats('splice')->addFalsePositive();
+    }
+  }
+}
+
+=head2 parseSTARJunctionLine
+
+=cut
+
+sub parseSTARJunctionLine {
+  my $line = shift;
+  my($chr,$start,$end,$strand,$intron_motif,$annotated,$uniq_cover,$multi_cover,$max_overhang) = split("\t",$line);
+  return {
+    chr           => $chr,
+    start         => $start-1, # Because STAR junctions file is 1-based
+    end           => $end-1, # Because STAR junctions file is 1-based
+    strand        => CracTools::Utils::convertStrand($strand),
+    intron_motif  => $intron_motif,
+    annotated     => $annotated,
+    uniq_cover    => $uniq_cover,
+    multi_cover   => $multi_cover,
+    max_overhang  => $max_overhang,
+  };
+}
+
+1;
