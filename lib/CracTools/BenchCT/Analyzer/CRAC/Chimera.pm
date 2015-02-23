dcb794e90b85c7b7547c2a1ac0361fdc6bf5b1e1
Jérôme Audoux <jerome.audoux@gmail.com>
2015-02-23 15:10:08 +0100

    Add score in positives chimera output string

--- lib/CracTools/BenchCT/Analyzer/CRAC/Chimera.pm
+++ lib/CracTools/BenchCT/Analyzer/CRAC/Chimera.pm
@@ -35,7 +35,7 @@ sub _init {
         $self->getStats('chimera')->addTruePositive(id => $true_chimera);
       } else {
         #print STDERR Dumper($chimera);
-        $self->getStats('chimera')->addFalsePositive(out_string => join("\t",($chimera->{chr1},$chimera->{pos1},$chimera->{strand1},$chimera->{chr2},$chimera->{pos2},$chimera->{strand2}),join(",",@{$chimera->{ids}})));
+        $self->getStats('chimera')->addFalsePositive(out_string => join("\t",($chimera->{chr1},$chimera->{pos1},$chimera->{strand1},$chimera->{chr2},$chimera->{pos2},$chimera->{strand2},$chimera->{score}),join(",",@{$chimera->{ids}})));
       }
     }
   }
